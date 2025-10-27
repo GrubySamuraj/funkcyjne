5de036778a533dd0ed4177870bcc2b7ad6d23251
Jakub <jakub.dragosz@o2.pl>
2025-10-27 17:34:11 +0100

    zadanie 4.5 done

--- zad1/src/main/scala/Main.scala
+++ zad1/src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 package app
+import scala.collection.mutable.ListBuffer
 object MinimalApplication extends cask.MainRoutes:
   override def host: String = "0.0.0.0"
   override def port: Int = 8080
@@ -31,6 +32,33 @@ object MinimalApplication extends cask.MainRoutes:
     val list1_2 = list1.zip(list2).map((item1, item2) => item1 + item2)
     val list1_2_3 = list1_2.zip(list3).map((item1, item2) => item1 + item2)
     list1_2_3
+  }
+
+  @cask.postJson("setHead")
+  def setHead(list: List[Int], element: Int) = {
+    val newListBuffer = ListBuffer[Int]()
+    newListBuffer.append(element)
+    for (lastElement <- list) {
+      newListBuffer.append(lastElement)
+    }
+    newListBuffer
+  }
+
+  @cask.postJson("append")
+  def append(list: List[Int], appendIndex: Int, elementToAdd: Int) = {
+    val newListBuffer = ListBuffer[Int]()
+    for ((element, index) <- list.zipWithIndex) {
+      if (index == appendIndex) {
+        newListBuffer.append(elementToAdd)
+      }
+      newListBuffer.append(element)
+    }
+
+    if (appendIndex == list.length) {
+      newListBuffer.append(elementToAdd)
+    }
+
+    newListBuffer
   }
 
   initialize()
