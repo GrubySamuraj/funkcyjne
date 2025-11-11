489c19c0f83c3aadd08a06490ac76e35265f29ab
Jakub Dragosz <jakub.dragosz@o2.pl>
2025-11-11 18:46:17 +0100

    3.0, 3.5 added

--- zad1/src/main/scala/Main.scala
+++ zad1/src/main/scala/Main.scala
@@ -1,5 +1,59 @@
 package app
 import scala.collection.mutable.ListBuffer
+
+
+class Node[T](var data: T, var prev: Node[T] = null, var next: Node[T] = null)
+class DoublyLinkedList[T](elements: List[T]) {
+  var head: Node[T] = null
+  var tailValue: Node[T] = null
+  var numOfElements = 0
+  for (elem <- elements) {
+    val newNode = new Node(elem)
+    if (head == null) {
+      head = newNode
+      tailValue = newNode
+    } else {
+      tailValue.next = newNode
+      newNode.prev = tailValue
+      tailValue = newNode
+    }
+    numOfElements+=1
+  }
+
+  def toList(): List[T] = {
+    var current = head
+    var returned = List[T]()
+    while (current != null) {
+      returned = returned :+ current.data
+      current = current.next
+    }
+    returned
+  }
+
+  def tail(): List[T] = {
+    if (head != null) {
+      head = head.next
+      if (head != null) head.prev = null
+      numOfElements -= 1
+    }
+    toList()
+  }
+
+  def drop(n: Int): List[T] = {
+    var current = head
+    var i = 0
+    var returned = List[T]()
+    while(current != null && i < n){
+      returned = returned :+ current.data
+      current = current.next
+      if (head != null) head.prev = null
+      i+=1
+      numOfElements-=1
+    }
+    returned
+  }
+}
+
 object MinimalApplication extends cask.MainRoutes:
   override def host: String = "0.0.0.0"
   override def port: Int = 8080
@@ -61,6 +115,23 @@ object MinimalApplication extends cask.MainRoutes:
   def makeSquares(list1:List[Int], list2:List[Int]) = {
     var list3 = list1.zip(list2).map((item1, item2)=> item1* item1 + item2*item2)
     list3
+  }
+
+  @cask.postJson("tail")
+  def testTails(list: List[Int]) = {
+    var doubleLinkedList = new DoublyLinkedList(list)
+    if (list == Nil){
+      Nil
+    }
+    else{doubleLinkedList
+      doubleLinkedList.tail()
+    }
+  }
+
+  @cask.postJson("drop")
+  def dropItems(list: List[Int], n: Int) = {
+    var doubleLinkedList = new DoublyLinkedList(list)
+    doubleLinkedList.drop(n)
   }
 
   initialize()
