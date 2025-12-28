e372831da2a052d809db2a020d6d71137ecd4dd0
Jakub Dragosz <jakub.dragosz@o2.pl>
2025-12-28 18:13:22 +0100

    zad 4.0 added

--- zad7/zad7/src/Main.hs
+++ zad7/zad7/src/Main.hs
@@ -7,15 +7,33 @@ import GHC.Generics (Generic)
 import System.Random (randomRIO)
 import Web.Scotty
 
+getRandomInt :: IO Int
+getRandomInt = randomRIO (0, maxBound :: Int)
+
+getRandomDouble :: IO Double
+getRandomDouble = randomRIO (0, 1) :: IO Double
+
 main :: IO ()
 main = scotty 3000 $ do
   get "/" $ do
     text "Witaj! To jest serwer Scotty postawiony od zera."
 
   get "/randomInt" $ do
-    liczba <- liftIO $ randomRIO (0, maxBound :: Int)
+    liczba <- liftIO getRandomInt
     json liczba
 
-  get "/randomFloat" $ do
-    numFloat <- liftIO $ randomRIO (0, 1 :: Double)
+  get "/randomDouble" $ do
+    numFloat <- liftIO getRandomDouble
     json numFloat
+
+  get "/randomPairs" $ do
+    i <- liftIO getRandomInt
+    d <- liftIO getRandomDouble
+    d2 <- liftIO getRandomDouble
+    d3 <- liftIO getRandomDouble
+
+    json
+      ( (i, d),
+        (d, i),
+        (d, d2, d3)
+      )
